c23e4d49a2d67cb2c6cf28924a54e4dab92e7381
Spencer Vatrt-Watts <asvw@uvic.ca>
2018-05-17 22:47:04 -0700

    Refactor LoggedIn and add tabBarOnPress navigation option

--- src/scenes/LoggedIn.js
+++ src/scenes/LoggedIn.js
@@ -1,46 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import { TabNavigator, TabBarBottom } from "react-navigation";
 
 import Map from "./Map.js";
 import Profile from "./Profile.js";
 import AddEvent from "./AddEvent/AddEvent";
 
-const LoggedIn = ({ onLogout }) => {
-  const TabNavigation = TabNavigator(
-    {
-      Home: {
-        screen: Map
-      },
-      AddEvent: {
-        screen: AddEvent
-      },
-      Profile: {
-        screen: props => {
-          return <Profile onLogout={onLogout} />;
-        }
-      }
-    },
-    {
-      navigationOptions: ({ navigation }) => ({
-        tabBarIcon: () => {
-          const { routeName } = navigation.state;
-          switch (routeName) {
-            case "Home":
-              break;
-            case "AddEvent":
-              break;
-            case "Profile":
-              break;
+class LoggedIn extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const TabNavigation = TabNavigator(
+      {
+        Home: {
+          screen: Map
+        },
+        AddEvent: {
+          screen: AddEvent
+        },
+        Profile: {
+          screen: props => {
+            return <Profile onLogout={this.props.onLogout} />;
           }
         }
-      }),
-      tabBarComponent: TabBarBottom,
-      tabBarPosition: "bottom",
-      animationEnabled: true,
-      swipeEnabled: true
-    }
-  );
+      },
+      {
+        navigationOptions: ({ navigation }) => ({
+          tabBarIcon: () => {
+            const { routeName } = navigation.state;
+            switch (routeName) {
+              case "Home":
+                break;
+              case "AddEvent":
+                break;
+              case "Profile":
+                break;
+            }
+          },
+          tabBarOnPress: ({ scene, jumpToIndex }) => {
+            console.log('onPress:', scene.route);
+            jumpToIndex(scene.index);
+          },
+        }),
+        tabBarComponent: TabBarBottom,
+        tabBarPosition: "bottom",
+        animationEnabled: true,
+        swipeEnabled: true
+      }
+    );
 
-  return <TabNavigation />;
-};
+    return <TabNavigation />;
+  }
+}
+
 export default LoggedIn;
