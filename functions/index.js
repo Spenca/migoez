c24b28070d919c7191854c8c0e309ac21d501bb6
Spencer Vatrt-Watts <asvw@uvic.ca>
2018-07-02 16:42:23 -0700

    Add first version of clean-up function (unfinished). Change firebase version, update database rules.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
-// const functions = require('firebase-functions');
-// const admin = require('firebase-admin');
-// const moment = require('moment');
-// admin.initializeApp();
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const moment = require('moment');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,18 +10,18 @@
 //  response.send("Hello from Firebase!");
 // });
 
-// exports.deleteOldItems = functions.database.ref('/events/{pushId}').onWrite((change) => {
-//   const ref = change.after.ref.parent; // reference to the parent
-//   const now = moment();
-//   const oldItemsQuery = ref.orderByChild('endTime').endAt(now);
-//   console.log(oldItemsQuery);
-//   return oldItemsQuery.once('value').then((snapshot) => {
-//     // create a map with all children that need to be removed
-//     const updates = {};
-//     snapshot.forEach(child => {
-//       updates[child.key] = null;
-//     });
-//     // execute all updates in one go and return the result to end the function
-//     return ref.update(updates);
-//   });
-// });
+exports.deleteOldItems = functions.database.ref('/events/{pushId}').onWrite((change) => {
+  const ref = change.after.ref.parent; // reference to the parent
+  const now = moment().valueOf();
+  const oldItemsQuery = ref.orderByChild('endTimeMilliseconds').endAt(now);
+  console.log(oldItemsQuery);
+  return oldItemsQuery.once('value').then((snapshot) => {
+    // create a map with all children that need to be removed
+    const updates = {};
+    snapshot.forEach(child => {
+      updates[child.key] = null;
+    });
+    // execute all updates in one go and return the result to end the function
+    return ref.update(updates);
+  });
+});
